bfc77f1dbbbb3bc58eccf06f135e3559abe27d44
shivaraj <shivaraj.feb@gmail.com>
2024-10-15 19:16:54 +0530

    noop file limit 10mb

--- src/pages/api/noop.ts
+++ src/pages/api/noop.ts
@@ -1,5 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// Set the size limit to 10MB
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
